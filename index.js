f6b23a73943de40b049ae1e67df7204f9e870b73
WongYC-66 <yengchien@hotmail.com>
2023-07-23 12:25:38 +0800

    add UI and javascript

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
+let playerScore = 0;
+let computerScore = 0;
+let gameStatus = 'play'
+
+document.querySelector('#rock').addEventListener('click', () => {
+    let result = playRound('rock', getComputerChoice())
+    document.querySelector('#gameText').textContent = result;
+    addPoints(result)
+})
+document.querySelector('#paper').addEventListener('click', () => {
+    let result = playRound('paper', getComputerChoice())
+    document.querySelector('#gameText').textContent = result;
+    addPoints(result)
+})
+document.querySelector('#scissors').addEventListener('click', () => {
+    let result = playRound('scissors', getComputerChoice())
+    document.querySelector('#gameText').textContent = result;
+    addPoints(result)
+})
+
+function addPoints(result){
+    if(! result) return console.log('Please start new round.')
+    if(result.split("!")[0] == 'You Won'){
+        playerScore++ ;
+        document.querySelector('#humanScore'). textContent = playerScore;
+    }
+    if(result.split("!")[0] == 'You Lose'){
+        computerScore++ ;
+        document.querySelector('#computerScore'). textContent = computerScore;
+    }
+    checkWinner();
+}
+
+function checkWinner(){
+    if(playerScore < 5 && computerScore < 5) return;
+    gameStatus = 'paused'
+    let game_msg = ""
+    if(playerScore > computerScore) game_msg =  "You won the game!" 
+    if(computerScore > playerScore) game_msg = "You lose the game!" 
+    if(computerScore == playerScore) game_msg = "Tie, try again?" 
+
+    // console.log(game_msg)
+    let parentNode = document.querySelector('#winnerText')
+    parentNode.textContent = game_msg;
+    // append reset button and logic
+    let childNode = document.createElement('button')
+    childNode.textContent = 'Another round?'
+    childNode.onclick = (() => {
+        playerScore = 0;
+        computerScore = 0;
+        document.querySelector('#humanScore'). textContent = playerScore;
+        document.querySelector('#computerScore'). textContent = computerScore;
+        parentNode.textContent = '';
+        document.querySelector('#gameText').textContent = '';
+        gameStatus = 'play'
+    })
+    parentNode.appendChild(childNode)
+}
+
 function getComputerChoice(){
     let num = Math.ceil(Math.random() * 3)
     return num === 1 ? 'Rock' :
@@ -6,6 +65,8 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection){
+    // console.log({playerSelection, computerSelection})
+    if(gameStatus == 'paused') return;
     let player = playerSelection[0].toUpperCase() + playerSelection.slice(1,)
     let comp = computerSelection[0].toUpperCase() + computerSelection.slice(1,)
 
@@ -19,29 +80,3 @@ function playRound(playerSelection, computerSelection){
     if(comp === player) return 'tie'
     if(player === afraid_of[comp]) return `You Lose! ${player} beaten by ${comp}`
 }
-
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for(let i = 1 ; i <= 5 ; i++){
-        console.log(i)
-        let userInput = prompt("Enter : rock/paper/scissors");
-        let result = playRound(userInput, getComputerChoice());
-        console.log(result)
-        if(result.split("!")[0] == 'You Won') playerScore++
-        if(result.split("!")[0] == 'You Lose') computerScore++
-    }
-
-    // Report out winner!
-    console.log({playerScore, computerScore})
-    let game_msg = ""
-    if(playerScore > computerScore) game_msg =  "You won the game!" 
-    if(computerScore > playerScore) game_msg = "You lose the game!" 
-    if(computerScore == playerScore) game_msg = "Tie, try again?" 
-
-    console.log(game_msg)
-    return game_msg
-}
-
-game()
